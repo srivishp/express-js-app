fd00f0f7d9a38f2d9493073c498920b85c8bb65f
Sri Vishnu <srivishp@gmail.com>
2025-07-10 18:34:07 +0530

    Import express in app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const http = require("http");
-const server = http.createServer();
+
+const express = require("express");
+
+const app = express();
+
+// Passing app to create the server as it is a valid handler.
+const server = http.createServer(app);
 
 server.listen(3000);
