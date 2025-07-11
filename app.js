a5d76ea0aaf845b3eab370a808ecf0bc92d4126c
Sri Vishnu <srivishp@gmail.com>
2025-07-11 15:01:11 +0530

    Add middleware

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
+// Creating middlewares
+console.log("Outside the middleware!");
 
+app.use((req, res, next) => {
+  console.log("In the first middleware!");
+  // next() is required for the execution to move forward into the next middleware
+  next();
+});
+app.use((req, res, next) => {
+  console.log("In the second middleware!");
+});
 // Passing app to create the server as it is a valid handler.
 const server = http.createServer(app);
 
